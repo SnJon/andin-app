a847de1ded6578c094fd1fed727fa4e9472a2840
SnJon <napstertm@gmail.com>
2023-05-31 00:11:13 +0900

    change FeedModel

--- app/src/main/java/ru/netology/nmedia/model/FeedModel.kt
+++ app/src/main/java/ru/netology/nmedia/model/FeedModel.kt
@@ -2,14 +2,19 @@ package ru.netology.nmedia.model
 
 import ru.netology.nmedia.dto.Post
 
-data class FeedModel(
-    val posts: List<Post> = emptyList(),
-    val loading: Boolean = false,
-    val error: Boolean = false,
-    val empty: Boolean = false,
-    val refreshing: Boolean = false,
-) {
-    fun isContentShowed(): Boolean {
-        return !loading && !error && !refreshing && !empty
+sealed interface FeedModelState {
+
+    data class Content(
+        val posts: List<Post> = emptyList(),
+        val error: FeedErrorEvent? = null
+    ) : FeedModelState {
+
+        fun isEmpty() = posts.isEmpty()
     }
+
+    object Loading : FeedModelState
 }
+
+data class FeedErrorEvent(
+    val itemIndex: Int? = null
+)
